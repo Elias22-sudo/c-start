29e0e478f904bedd42aac0c541911a10abdba002
Elias22-sudo <mengomatheus22@gmail.com>
2025-10-01 21:52:52 -0300

    Perguntas e respostas

--- Fundamnetos/Program.cs
+++ Fundamnetos/Program.cs
@@ -1,7 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-#region primeiro programa
+﻿#region primeiro programa
 
-Console.WriteLine("Hello, world!");
+//Console.WriteLine("Hello, world!");
 
 #endregion
- pode não
+
+using System.IO.Pipes;
+
+//string nome = "José";
+//int idade = 30;
+//double altura = 1.8;
+//Console.WriteLine($"Seu nome é {nome}\nSeja bem-vinda(o)! nesse hotel {nome} você terá uma experiência inesquecível!.\nVocê tem {idade} anos e sua altura é {altura} m");
+
+Console.WriteLine("Digite seu nome: ");
+string nome = Console.ReadLine()!;
+Console.WriteLine($"Seu nome é {nome}");
+
+Console.WriteLine("Qual sua idade?");
+string idade = Console.ReadLine()!;
+Console.WriteLine($"Você tem {idade} anos");
+
+Console.WriteLine($"Você é novo no jogo?");
+string premissas = Console.ReadLine()!;
+Console.WriteLine($"voce é novo no jogo! {premissas}");
