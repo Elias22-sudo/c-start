419453ea11aef57409f0f21afcd7d4dd84848ecf
Elias22-sudo <mengomatheus22@gmail.com>
2025-10-03 22:07:33 -0300

    aticidades com váriaveis e operações arítimeticas

--- Fundamnetos/Program.cs
+++ Fundamnetos/Program.cs
@@ -1,24 +1,54 @@
-﻿#region primeiro programa
+﻿//#region primeiro programa
 
 //Console.WriteLine("Hello, world!");
 
-#endregion
+//#endregion
 
-using System.IO.Pipes;
+//using System.IO.Pipes;
 
 //string nome = "José";
 //int idade = 30;
 //double altura = 1.8;
 //Console.WriteLine($"Seu nome é {nome}\nSeja bem-vinda(o)! nesse hotel {nome} você terá uma experiência inesquecível!.\nVocê tem {idade} anos e sua altura é {altura} m");
 
-Console.WriteLine("Digite seu nome: ");
-string nome = Console.ReadLine()!;
-Console.WriteLine($"Seu nome é {nome}");
 
-Console.WriteLine("Qual sua idade?");
-string idade = Console.ReadLine()!;
-Console.WriteLine($"Você tem {idade} anos");
 
-Console.WriteLine($"Você é novo no jogo?");
-string premissas = Console.ReadLine()!;
-Console.WriteLine($"voce é novo no jogo! {premissas}");
+
+
+//#region 
+
+
+//int idade;
+//string username;
+
+//Console.Write("digite seu nome: ");
+//username = Console.ReadLine()!;
+//Console.Write("digite sua idade: ");
+//idade = int.Parse(Console.ReadLine()!);
+//Console.WriteLine($"Olá {username}, você tem {idade} anos!");
+//#endregion
+
+
+
+
+
+//int B = 1 * 2;
+//Console.WriteLine(B);
+//int C = 2 + 2;
+//Console.WriteLine(C);
+//int D = 15 / 3;
+//Console.WriteLine(D);
+//int e = 12 - 5;
+//Console.WriteLine(e);
+//int t = 11 % 7;
+//Console.WriteLine(t);
+//int f = 9 % 5;
+//Console.WriteLine(f);
+
+int j = 64;
+double n = 3.14 * (j * j); 
+Console.WriteLine($" 3.14 * {j}² = {n} ");
+
+int a = 190;
+double b = (a - 32) * 5 / 9;
+Console.WriteLine($"{a}°F é igual a {b}°C");
