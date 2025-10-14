47df5fce4c528ba6ed6693084c34b0ae46a728e1
Elias22-sudo <mengomatheus22@gmail.com>
2025-10-13 20:11:09 -0400

    resolvido

---
+++ atividade_1/Program.cs
@@ -0,0 +1,149 @@
+﻿#region atividade 1
+
+
+// double num1, num2, num3, heron, area;
+// Console.WriteLine("Primeiro lado do triangulo:");
+// num1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Segundo lado do triangulo:");
+// num2 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Terceiro lado do triangulo:");
+// num3 = Convert.ToDouble(Console.ReadLine());
+// heron = (num1 + num2 + num3) / 2;
+// area = Math.Sqrt(heron * (heron - num1) * (heron - num2) * (heron - num3));
+// Console.WriteLine($"A area do triangulo é: {area}");
+
+#endregion
+
+#region atividade 2
+
+
+
+// float horas, valor, salario;
+// Console.WriteLine("Digite a quantidade de horas trabalhadadas: ");
+// horas = float.Parse(Console.Writeline()!);
+// Console.WriteLine("Digite o valor que recebe por hora: ");
+// valor = float.Parse(Console.Writeline()!);
+// salario = horas * valor;
+// Console.WriteLine($"O salario total é: {salario}");
+
+
+
+
+#endregion
+
+#region atividade 3
+
+
+// double km, distância, horas;
+// Console.WriteLine("Distância em km:");
+// km = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Horas gastas:");
+// horas = Convert.ToDouble(Console.ReadLine());
+// distância = km * horas;
+// Console.WriteLine($"A velocidade média é: {distância} km/h");
+
+
+#endregion
+
+#region atividade 4
+
+
+
+double number1, number2, number3, esfera;
+Console.WriteLine("Primeiro numero:");
+number1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Segundo numero:");
+number2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Terceiro numero:");
+number3 = Convert.ToDouble(Console.ReadLine());
+esfera = 4 / 3 * 3.14 * Math.Pow(number3, 3);
+Console.WriteLine($"O volume da esfera é: {esfera}");
+
+
+#endregion
+
+#region atividade 5
+
+
+
+
+// float fahrenheit, celsius;
+// Console.WriteLine("Temperatura em Fahrenheit:");
+// fahrenheit = float.Parse(Console.ReadLine());
+// celsius = (fahrenheit - 32) * 5 / 9;
+// Console.WriteLine($"A temperatura em Celsius é: {celsius}°C");
+
+
+#endregion
+
+
+#region atividade 6
+
+
+
+
+// double one, two, three, ponderada;
+// Console.WriteLine("O valor da primeira nota:");
+// one = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("O valor da segunda nota:");
+// two = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("O valor da terceira nota:");
+// three = Convert.ToDouble(Console.ReadLine());
+// ponderada = (one * 2 + two * 3 + three * 5) / 10;
+// Console.WriteLine($"A media ponderada é: {ponderada}");
+
+#endregion
+
+#region atividade 7
+
+
+// decimal number1, number2, number3, imposto;
+// Console.WriteLine("O valor do primeiro salario:");
+// number1 = Convert.ToDecimal(Console.ReadLine());
+// Console.WriteLine("O valor do segundo salario:");
+// number2 = Convert.ToDecimal(Console.ReadLine());
+// Console.WriteLine("O valor do terceiro salario:");
+// number3 = Convert.ToDecimal(Console.ReadLine());
+// imposto = (number1 + number2 + number3) * 0.07m;
+// Console.WriteLine($"O valor total do imposto é: {imposto}");
+
+#endregion
+
+#region atividade 8
+
+// int segundos, minutos, horas, resto;
+// Console.WriteLine("A quantidade de segundos:");
+// segundos = Convert.ToInt32(Console.ReadLine());
+// horas = segundos / 3600;
+// resto = segundos % 3600;
+// minutos = resto / 60;
+// resto = resto % 60;
+// Console.WriteLine($"{horas} horas, {minutos} minutos e {resto} segundos");
+#endregion
+
+#region atividade 9
+
+// #region atividade 9
+// double hipotenusa, cateto1, cateto2;
+// Console.WriteLine("Primeir cateto:");
+// cateto1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Segundo cateto:");
+// cateto2 = Convert.ToDouble(Console.ReadLine());
+// hipotenusa = Math.Sqrt(Math.Pow(cateto1, 2) + Math.Pow(cateto2, 2));
+// Console.WriteLine($"A hipotenusa é: {hipotenusa}");
+
+
+#endregion
+
+#region atividade 10
+
+// float distância, metros, tempo, segundos;
+// Console.WriteLine("Distância em metros:");
+// distância = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Tempo em segundos:");
+// tempo = Convert.ToDouble(Console.ReadLine());
+// metros = distância / tempo;
+// segundos = distância / 1000 / (tempo / 3600);
+// Console.WriteLine($"A velocidade média é: {segundos} km/h");
+
+#endregion
