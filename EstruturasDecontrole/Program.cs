af47f09312f7b921c86a4f3a037545cc6dde8ace
Elias22-sudo <mengomatheus22@gmail.com>
2025-10-17 21:01:48 -0400

    aticidades com váriaveis e operações arítimeticas

---
+++ EstruturasDecontrole/Program.cs
@@ -0,0 +1,42 @@
+﻿#region está chovendo
+// Boolean estáchovendo = false;
+// Console.WriteLine("Saindo de casa");
+// if (estáchovendo)
+// {
+//     Console.WriteLine("Levo o guarda-chuva");
+// }
+// else{
+//     Console.WriteLine("Continuo o passeio");
+// }
+#endregion
+
+
+
+#region positivo e negativo
+
+// double numerointeiro = -13 ;
+// Console.WriteLine($"Se {numerointeiro} for maior ou menor que zero é:");
+// if( numerointeiro > 0 )
+// {
+//     Console.WriteLine("é positivo");
+// }
+// else {
+
+//     Console.WriteLine("é negativo");
+// }
+
+
+#endregion
+
+
+#region escola
+Boolean indoparaescola = false;
+Console.WriteLine("Indo para a escola");
+if(indoparaescola)
+{
+    Console.WriteLine("Vá pra escola");
+}
+else{
+    Console.WriteLine("Fica em casa");
+}
+#endregion
