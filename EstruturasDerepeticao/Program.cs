1f60b1575b008422456936e10234dc09c4a725e2
Elias22-sudo <mengomatheus22@gmail.com>
2025-11-03 21:01:54 -0400

    atividades

--- EstruturasDerepeticao/Program.cs
+++ EstruturasDerepeticao/Program.cs
@@ -17,9 +17,59 @@
 
 
 
-for (int n = 0; n <= 12; n++)
-{
-    Console.Write($" ");
+
+// for (int n = 0; n <= 12; n++)
+// {
+//     Console.Write($" ");
+// }
+
+
+
+
+#endregion
+
+#region Do while
+
+
+
+// char opcao;
+// char continuar;
+
+// do
+// {
+//     Console.WriteLine("-----------------------");
+//     Console.WriteLine("Escolha a opcao");
+//     Console.WriteLine("a) Somar");
+//     Console.WriteLine("b) Subtracao");
+//     Console.WriteLine("opcao: ");
+//     opcao = Console.ReadKey().KeyChar;
+
+//     switch (opcao)
+//     {
+//         case 'a': Console.WriteLine("Somando número"); break;
+//         case 'b': Console.WriteLine("Subtraindo número"); break;
+//         default: Console.WriteLine("Inválido"); break;
+//     }
+
+//     Console.WriteLine("================================");
+//     Console.WriteLine("Deseja continuar? \n a)Sim \nb)Não");
+//     continuar = Console.ReadKey().KeyChar; 
+// }while (continuar == 'a');
+
+
+#endregion
+
+#region Fatorial
+
+
+int numero;
+numero = int.Parse(Console.ReadLine()!);
+
+
+for ( int n= 1; n <=3; n++ ){
+
+
+
 }
 
 
