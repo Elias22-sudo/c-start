30b162bc4efe87625e6a5508bdefa1eb61c4c066
Elias22-sudo <mengomatheus22@gmail.com>
2025-11-14 20:58:23 -0400

    teste de função

---
+++ Funcoes/Program.cs
@@ -0,0 +1,90 @@
+﻿#region atividade 
+
+
+
+string continuar;
+
+
+
+// do
+// {
+//     Console.WriteLine("--------------------------------------");
+//     Console.WriteLine("Conversor de Temperatura");
+//     Console.WriteLine("Escolha a opção de conversão:");
+//     Console.WriteLine("1 - Celsius para Fahrenheit");
+//     Console.WriteLine("2 - Fahrenheit para Celsius");
+//     Console.WriteLine("3 - Celsius para Kelvin");
+//     Console.WriteLine("4 - Kelvin para Celsius");
+//     Console.Write("Opção: ");
+
+//     string opcao = Console.ReadLine()!;
+//     double temperatura, resultado;
+
+//     switch (opcao)
+//     {
+//         case "1":
+//             Console.WriteLine("--------------------------------------");
+//             Console.Write("Digite a temperatura em Celsius: ");
+//             temperatura = double.Parse(Console.ReadLine()!);
+//             resultado = (temperatura * 9 / 5) + 32;
+//             Console.WriteLine($"{temperatura}°C = {resultado:F2}°F");
+//             Console.WriteLine("--------------------------------------");
+//             break;
+
+//         case "2":
+//             Console.WriteLine("--------------------------------------");
+//             Console.Write("Digite a temperatura em Fahrenheit: ");
+//             temperatura = double.Parse(Console.ReadLine()!);
+//             resultado = (temperatura - 32) * 5 / 9;
+//             Console.WriteLine($"{temperatura}°F = {resultado:F2}°C");
+//             Console.WriteLine("--------------------------------------");
+//             break;
+
+//         case "3":
+//             Console.WriteLine("--------------------------------------");
+//             Console.Write("Digite a temperatura em Celsius: ");
+//             temperatura = double.Parse(Console.ReadLine()!);
+//             resultado = temperatura + 273.15;
+//             Console.WriteLine($"{temperatura}°C = {resultado:F2}K");
+//             Console.WriteLine("--------------------------------------");
+//             break;
+
+//         case "4":
+//             Console.WriteLine("--------------------------------------");
+//             Console.Write("Digite a temperatura em Kelvin: ");
+//             temperatura = double.Parse(Console.ReadLine()!);
+//             resultado = temperatura - 273.15;
+//             Console.WriteLine($"{temperatura}K = {resultado:F2}°C");
+//             Console.WriteLine("--------------------------------------");
+//             break;
+
+//         default:
+//             Console.WriteLine("Opção inválida!");
+//             break;
+//     }
+
+//     Console.Write("Deseja fazer outra conversão? (S/N): ");
+//     continuar = Console.ReadLine()!;
+//     Console.WriteLine();
+
+// } while (continuar.Equals("S", StringComparison.OrdinalIgnoreCase));
+
+// Console.WriteLine("Programa encerrado.");
+
+
+
+
+
+
+
+
+
+#endregion
+
+
+
+
+
+
+
+
